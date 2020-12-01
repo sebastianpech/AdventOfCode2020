bab7b93e221caeb55064d590d33dfa389c82a62c
Sebastian Pech <sebastian.pech@me.com>
2020-12-01 10:42:47 +0100

    Add some improvements to part 2

--- 1.jl
+++ 1.jl
@@ -6,3 +6,18 @@ first(x*y for x in s, y in s, z in s if x+y==2020)
 # part 2
 first(x*y*z for x in s, y in s, z in s if x+y+z==2020)
 
+# Playing around with improving part 2
+using BenchmarkTools
+
+function part2(s)
+    _s = sort(s)
+    for x in _s
+        for y in _s
+            x+y >= 2020 && break
+            for z in _s
+                x+y+z > 2020 && break
+                x+y+z == 2020 && return x*y*z
+            end
+        end
+    end
+end
