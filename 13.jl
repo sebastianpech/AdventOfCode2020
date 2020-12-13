4571f5eef12d9553ba35b807b30706260f14e4ec
Sebastian Pech <sebastian.pech@me.com>
2020-12-13 13:31:58 +0100

    Improve day 13

--- 13.jl
+++ 13.jl
@@ -1,5 +1,3 @@
-inp = split("""939
-            7,13,x,x,59,x,31,19""","\n")
 inp = readlines("13.input")
 
 # part 1
@@ -14,16 +12,13 @@ waittime[minid]*busid
 # part 2
 
 function merge_periodic(na,ma,nb,mb,Δ=0; ignore_period=false)
-    ta = 0
-    tb = 0
+    ta = div(nb-na,ma,RoundUp)
     n = 0
     found_start = false
     while true
         target = na+ma*ta
-        while nb+tb*mb < target+Δ
-            tb += 1
-        end
-        if nb+tb*mb == target+Δ 
+        tb = (target+Δ-nb)/mb
+        if floor(tb) == tb
             found_start && return n, target-n
             n = target
             if ignore_period
@@ -38,11 +33,11 @@ end
 function part02(buses)
     Δ = [i-1 for i in 1:length(buses) if buses[i] !== nothing]
     buses = filter(!(==(nothing)),buses)
-    init = merge_periodic.(Ref(0),Ref(buses[1]),Ref(0),buses[2:end],Δ[2:end])
-    while length(init) > 2
-        init = [ merge_periodic(init[1]...,init[i]...,0) for i in 2:length(init)]
+    basef = merge_periodic(0,buses[1],0,buses[2],Δ[2])
+    for i in 3:length(buses)-1
+        basef = merge_periodic(basef...,0,buses[i],Δ[i])
     end
-    merge_periodic(init[1]...,init[2]...,0, ignore_period=true)[1]
+    return merge_periodic(basef...,0,buses[end],Δ[end], ignore_period=true)[1]
 end
 
 buses = tryparse.(Int,split(inp[2],","))
